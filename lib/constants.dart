79cba32d608b44993704015eff5eb0fe0fda4d8d
Leif Etter <leif.etter@gmx.de>
2023-11-23 01:03:26 +0100

    add constants file containing class with colors

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GrowableColors {
+  static const lightGreen = Color(0xff5E936F);
+  static const green = Color(0xff3E6D4C);
+  static const yellowGreen = Color(0xffFEFEE6);
+  static const pink = Color(0xffF7CBBC);
+}
